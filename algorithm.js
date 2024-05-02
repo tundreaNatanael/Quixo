f46d278fcc1f1a7ff14876da25501441f80969e0
nathan.tundrea <natanael.tundrea@emanuel.ro>
2024-05-02 12:27:17 +0300

    playing backAlgorithm

--- algorithm.js
+++ algorithm.js
@@ -3,8 +3,9 @@ const boardSize = 5; //dimensiunea tablei de joc de 5x5
 const squareSize = 100; //dimensiunea unui patrat de 100x100 pixeli
 const squareColor = "#8f2e04"; //culoarea patratelor
 
-const game = {
-    playerTurn: 1
+const game = { //obiectul care contine informatii despre joc
+    playerTurn: 1, //jucatorul curent (1 - X, 2 - 0)
+    antSquare: 0 //patratul pe care s-a dat click anterior - daca e 0 atuncea inseamna ca nu s-a dat click anterior ([i, j])
 };
 
 //functia de initializare a matrix - se executa la incarcarea paginii
@@ -23,10 +24,60 @@ function initSquares() {
     }
 }
 
-// test the program in node.js
-// initSquares();
-// for(let i=0; i<boardSize; i++){
-//     for(let j=0; j<boardSize; j++)
-//         process.stdout.write("("+matrix[i][j].x+", "+matrix[i][j].y+", "+matrix[i][j].color+"), ");
-//     console.log()
-// }
+function clickedSquare(i, j) {
+    if(
+        game.antSquare == 0 && //sa nu fie niciun patrat selectat anterior
+        (matrix[i][j].side == 0 || matrix[i][j].side == game.playerTurn) && //patratul sa nu fie al adversarului
+        (i == 0 || i == boardSize - 1 || j == 0 || j == boardSize - 1) //patratul sa fie pe margine
+    ) 
+    {
+        game.antSquare = [i, j]; //game.antSquare primesc coordonatele patratului selectat
+        return true
+    }
+    else{
+        if(
+            !(game.antSquare[0] == i && game.antSquare[1] == j) && //daca patratul selectat anterior nu e acelasi cu cel selectat acum
+            (
+                (game.antSquare[0] == i && (j == 0 || j == boardSize - 1)) ||
+                (game.antSquare[1] == j && (i == 0 || i == boardSize - 1))
+            ) //daca e pe aceiasi linie sau coloana cu patratul selectat anterior
+        ){
+            interchange(game.antSquare, [i, j]) //facem schimbarea
+            game.playerTurn = 3 - game.playerTurn; //schimbam jucatorul
+            game.antSquare = 0; //deselctam patratul
+            return true
+        }
+    }
+    return false
+}
+
+function interchange(chosen, target){
+    if(chosen[0] == target[0]){ //daca schimbarea se pe linie
+        if(chosen[1] < target[1]){ //daca schimbarea se face spre dreapta
+            for(let j=target[1]-1; j>=chosen[1]; j--){
+                matrix[chosen[0]][j].side = matrix[chosen[0]][j+1].side
+            }
+            matrix[target[0]][target[1]].side = game.playerTurn
+        }
+        else{ //daca schimbarea se face spre stanga
+            for(let j=target[1]+1; j<=chosen[1]; j--){
+                matrix[chosen[0]][j].side = matrix[chosen[0]][j-1].side
+            }
+            matrix[target[0]][target[1]].side = game.playerTurn
+        }
+    }
+    else{ //daca schimbarea se face pe coloana
+        if(chosen[0] < target[0]){ //daca schimbarea se face in jos
+            for(let i=target[0]-1; i>=chosen[0]; i--){
+                matrix[i][chosen[1]].side = matrix[i+1][chosen[1]].side
+            }
+            matrix[target[0]][target[1]].side = game.playerTurn
+        }
+        else{ //daca schimbarea se face in sus
+            for(let i=target[0]+1; i<=chosen[0]; i++){
+                matrix[i][chosen[1]].side = matrix[i-1][chosen[1]].side
+            }
+            matrix[target[0]][target[1]].side = game.playerTurn
+        }
+    }
+}
---
+++ testConsole.js
@@ -0,0 +1,21 @@
+// test the program in node.js
+initSquares();
+console.log("first click: ", clickedSquare(3, 0));
+console.log("second click: ", clickedSquare(0, 0));
+console.log("player: ", game.playerTurn);
+for(let i=0; i<boardSize; i++){
+    let row = ""
+    for(let j=0; j<boardSize; j++)
+        row += matrix[i][j].side + " "
+    console.log(row)
+}
+
+console.log("first click: ", clickedSquare(0, 2));
+console.log("second click: ", clickedSquare(4, 2));
+console.log("player: ", game.playerTurn);
+for(let i=0; i<boardSize; i++){
+    let row = ""
+    for(let j=0; j<boardSize; j++)
+        row += matrix[i][j].side + " "
+    console.log(row)
+}
