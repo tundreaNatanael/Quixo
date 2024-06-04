ac55ef74aed6b2cce142b2c9ad0fc7ca731a2622
Giorgiana <giorgiana.dinu@emanuel.ro>
2024-06-04 08:34:59 +0300

    checkReset

--- check.js
+++ check.js
@@ -1,17 +1,19 @@
 //fisierul care contine functiile care verifica anumite lucruri
 
 //functia care verifica daca mouse-ul se afla in interiorul tablei de joc si apeleaza functia clickedSquare calculand i si j din coordonatele mouseX si mouseY
-checkInsideBoard(){
+checkInsideBoard();{
     if(
         (startBoardX <= mouseX && mouseX <= startBoardX + boardSize * squareSize) &&
         (startBoardY <= mouseY && mouseY <= startBoardY + boardSize * squareSize)
-    )
-        clickedSquare(Math.floor((mouseY - startBoardY)/boardSize), Math.floor((mouseX - startBoardX)/boardSize))
+    ){
+        clickedSquare(Math.floor((mouseY - startBoardY)/squareSize), Math.floor((mouseX - startBoardX)/squareSize));
+    }
 }
-checkReset(){
+checkReset();{
     if(
-        (stardBoardX + 4 *squareSize <= mouseX && <= startBoardX + 4 *squareSize + playerVsBotEasyButtonSize) &&
-        (stardBoardY + 4 *squareSize <= mouseY && <= startBoardY + 4 *squareSize + playerVsBotEasyButtonSize)
-    )
-        clickedSquare(Math.floor((mouseY - startBoardY + 4 *squareSize)/boardSize), Math.floor((mouseX - startBoardX + 4 *squareSize)/boardSize))
+        (startBoardX + 4 *squareSize <= mouseX && mouseX <= startBoardX + 4 *squareSize + playerVsBotEasyButtonSize) &&
+        (startBoardY + 4 *squareSize <= mouseY && mouseY <= startBoardY + 4 *squareSize + playerVsBotEasyButtonSize)
+    ){
+        clickedSquare(Math.floor((mouseY - (startBoardY + 4 *squareSize)/squareSize)), Math.floor((mouseX - (startBoardX + 4 *squareSize)/squareSize)));
+    }
 }
