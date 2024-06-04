74a7ce998bdcbea122575fdd0970843da9e82924
Giorgiana <giorgiana.dinu@emanuel.ro>
2024-06-04 15:35:40 +0300

    check

--- check.js
+++ check.js
@@ -1,7 +1,7 @@
 //fisierul care contine functiile care verifica anumite lucruri
 
 //functia care verifica daca mouse-ul se afla in interiorul tablei de joc si apeleaza functia clickedSquare calculand i si j din coordonatele mouseX si mouseY
-checkInsideBoard();{
+checkInsideBoard(){
     if(
         (startBoardX <= mouseX && mouseX <= startBoardX + boardSize * squareSize) &&
         (startBoardY <= mouseY && mouseY <= startBoardY + boardSize * squareSize)
@@ -9,7 +9,7 @@ checkInsideBoard();{
         clickedSquare(Math.floor((mouseY - startBoardY)/squareSize), Math.floor((mouseX - startBoardX)/squareSize));
     }
 }
-checkReset();{
+checkReset(){
     if(
         (startBoardX + 4 *squareSize <= mouseX && mouseX <= startBoardX + 4 *squareSize + playerVsBotEasyButtonSize) &&
         (startBoardY + 4 *squareSize <= mouseY && mouseY <= startBoardY + 4 *squareSize + playerVsBotEasyButtonSize)
